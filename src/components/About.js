cd9aff8b3c8ba0ea7e2f3c0ada99e4bac39ad042
Dalima Sahu <dalima62657@gmail.com>
2025-03-19 16:31:55 +0530

    About me and herosection changed

--- src/components/About.js
+++ src/components/About.js
@@ -1,38 +1,62 @@
-import React from 'react';
-import "../styles/About.css"
+import React from "react";
+import "../styles/About.css";
 import { motion } from "framer-motion";
-import ProfileImg from '../images/profile_me.jpg'
+import ProfileImg from "../images/profile_me.jpg";
 
-const About = () => {  
-  
-  const horizontal ={
-    x:0, 
-    opacity: 1, 
-    transition:{type: 'spring', duration: 2,bounce: 0.3}
-  }
+const About = () => {
+  const horizontal = {
+    x: 0,
+    opacity: 1,
+    transition: { type: "spring", duration: 2, bounce: 0.3 },
+  };
 
   return (
-      <>
-          <div  className="about" id='about'>
-              <div className="container">
-                  <motion.div initial={{x: '-100%', opacity: 0}} whileInView={horizontal} viewport={{ once: true }} className="heading">
-                    <p className="heading-sub-text">Who I am</p>
-                    <p className='heading-text'>About Me</p>
-                  </motion.div>
-                  <div className="split-about">
-                    <motion.div initial={{x: '-100%', opacity: 0}} whileInView={horizontal} className="about-content">
-                        <p>Hello! My name is Peace and I enjoy creating things that live on the internet. My interest in web development started back in 2018 when I decided to try out for my first school project — turns out putting together a simple website taught me a lot about HTML & CSS! </p>
-                        <br />
-                        <p> Since then, I've had the opportunity to collaborate with some talented professionals, which has sharpened my skills and helped me grow faster. These days, I'm all about building accessible and inclusive digital products that make a difference. I also enjoy dabbling in design and illustration whenever I can.</p>
-                    </motion.div>
-                    <motion.div initial={{x: '50', opacity: 0}} whileInView={horizontal}  className='about-img'>
-                        <img src={ProfileImg} alt="Profile" />
-                    </motion.div>
-                  </div>
-              </div>
+    <>
+      <div className="about" id="about">
+        <div className="container">
+          <motion.div
+            initial={{ x: "-100%", opacity: 0 }}
+            whileInView={horizontal}
+            viewport={{ once: true }}
+            className="heading"
+          >
+            <p className="heading-sub-text">Who I am</p>
+            <p className="heading-text">About Me</p>
+          </motion.div>
+          <div className="split-about">
+            <motion.div
+              initial={{ x: "-100%", opacity: 0 }}
+              whileInView={horizontal}
+              className="about-content"
+            >
+              <p>
+                {" "}
+                I am a final-year student passionate about programming,
+                specializing in Android development with skills in Java, Kotlin,
+                and Python. Additionally, I have experience with web
+                technologies, including HTML, CSS, JavaScript, and React. My
+                major project involves building a grammar error detection tool
+                using machine learning, which is integrated into an Android app.{" "}
+              </p>
+              <br />
+              <p>
+                {" "}
+                I thrive on challenges and continuously explore emerging
+                technologies to deliver impactful solutions.
+              </p>
+            </motion.div>
+            <motion.div
+              initial={{ x: "50", opacity: 0 }}
+              whileInView={horizontal}
+              className="about-img"
+            >
+              <img src={ProfileImg} alt="Profile" />
+            </motion.div>
           </div>
-      </>
-  )
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default About;
