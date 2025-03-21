4f497d842a5c74d6df765fdec0d9c5bb1813dbde
Dalima Sahu <dalima62657@gmail.com>
2025-03-21 17:35:23 +0530

    navbar changed

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -150,9 +150,7 @@ const Navbar = ({ nav, handleNav }) => {
             </Link>
           </ul>
           <div className="copy">
-            <small className="copyright">
-              © Copyright ©2025 <br /> All rights reserved |
-            </small>
+            <small className="copyright"></small>
           </div>
         </motion.div>
       </motion.nav>
